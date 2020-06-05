9c8778a9ba6b107c01aa817811ec87114c54dcbc
Stéphane Lasmartres <SLasmartres@idt.pf>
2020-06-04 15:36:24 -1000

    add F# implementation

---
+++ fsharp/catdance/Controllers/HomeController.fs
@@ -0,0 +1,21 @@
+﻿namespace catdance
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+open HomeModels
+open UrlsStore
+
+type HomeController (logger : ILogger<HomeController>) =
+    inherit Controller()
+
+    member this.Index () =
+        let rng = Random();
+        let urls = UrlsStore.getUrls
+        let url = urls.[rng.Next(urls.Length)]
+        let model = CatUrlModel(url)
+        this.View(model)
